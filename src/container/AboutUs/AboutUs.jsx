30d009eb06afbd1aae0d435f43b0853f48f63698
Aayush Shrestha <aayushshrestha@Aayushs-MacBook-Pro.local>
2022-09-06 16:10:58 -0400

    finished about us

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {images} from '../../constants';
+import { images } from '../../constants';
 
 import './AboutUs.css';
 
@@ -11,9 +11,38 @@ const AboutUs = () => (
     </div>
 
     <div className='app__aboutus-content flex__center'>
-      <div className=''></>
+
+
+      <div className='app__aboutus-content_about'>
+        <h1 className='headtext__cormorant'> About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p classname='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <button type="button" className='custom__button'>Know More</button>
+        <div />
+      </div>
+
+
+      
+      <div className='app__aboutus-content_knife flex_center'>
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+
+      <div className='app__aboutus-content_history'>
+        <h1 className='headtext__cormorant'> Our history</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p classname='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <button type="button" className='custom__button'>Know More</button>
+        <div />
+      </div>
+
     </div>
+
+
+
   </div>
+
+
 );
 
 export default AboutUs;
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
+import { SubHeading, MenuItem } from '../../components';
+import { images, data } from '../../constants'; 
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className='app__specialMenu flex__center section__padding' id='menu'>
+
+
+    <div className='app__specialMeny-title'>
+      <SubHeading title="Menu that fits your palatte"/>
+      <h1 className='headtext__comorant'>Today&apos;s Special</h1>
+    </div>
+
+    <div className='app__specialMenu-menu_wine flex__center'>
+      <p className='app__specialMenu-menu_heading'>Wine & Beer</p>
+      <div className='app__specialMenu_meny_items'>
+        {data.wines.map((wine,index)=>(
+          <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags} />
+        ))}
+      </div>
+
+    </div>
   </div>
 );
 
