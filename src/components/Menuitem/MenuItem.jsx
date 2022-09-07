df36d16b6b6e643dca931fa36b37b3beb5b02a5d
Aayush Shrestha <aayushshrestha@Aayushs-MacBook-Pro.local>
2022-09-07 16:47:29 -0400

    added menu for drinks

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({ title, price, tags }) => (
+  <div className='aoo__menuitem'>
+    <div className='app__menyitem-head'>
+      <div className='app__menuitens-name'>
+        <p className='p__cormorant' style={{ color: '#DCCA87' }}>{title}</p>
+      </div>
+      <div className="app__menuitem-dash" />
+      <div className="app__menuitem-price">
+        <p className="p__cormorant">{price}</p>
+      </div>
+    </div>
+    <div className="app__menuitem-sub">
+      <p className="p__opensans" style={{ color: '#AAAAAA' }}>{tags}</p>
+    </div>
   </div>
 );
 
